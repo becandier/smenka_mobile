6ebe8f9faccc13e9f19644b4e64aece35c1c9d63
Your Name <imanov.sh@mail.ru>
2026-04-06 21:11:34 +0300

    feat: barrel file профиля + экспорт в _features

--- lib/pages/_features.dart
+++ lib/pages/_features.dart
@@ -2,5 +2,8 @@ export 'auth/_auth.dart';
 export 'debug/_debug.dart';
 export 'home/_home.dart';
 export 'main_router/_main_router.dart';
+export 'profile/_profile.dart';
+export 'shift_detail/_shift_detail.dart';
+export 'shift_history/_shift_history.dart';
 export 'shift_tracker/_shift_tracker.dart';
 export 'verify/_verify.dart';
---
+++ lib/pages/profile/_profile.dart
@@ -0,0 +1,4 @@
+export 'cubit/profile_cubit.dart';
+export 'cubit/profile_state.dart';
+export 'view/profile_page.dart';
+export 'widgets/edit_profile_modal.dart';
